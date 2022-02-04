da778cdd89c0c8eed7f4e7211f0affb4f3f51eff
Jon Skinner <jon.a.skinner1@gmail.com>
2022-02-04 12:38:57 -0500

    first commit skeleton;

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
+
+gulp.task("default", () => {
+    gulp.src("src/**/*.js").pipe(eslint()).pipe(eslint.format());
+    gulp.src("src/**/*.js").pipe(babel()).pipe(gulp.dest("dist"));
+    gulp.src("src/**/*.pug").pipe(gulp.dest("dist"));
+});
